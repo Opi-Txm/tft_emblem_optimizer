1261cb031ce071590f5743574b4c9ba3ea7a6d0c
Otakushima <ota.takushima@tum.de>
2025-06-24 15:41:48 +0200

    json of champions/traits

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+
+team_size = 9
+emblems = ["Techie", "Vanguard", "Street Demon", "Golden Ox", "Slayer"]
+champions = ["Brand", "Jarvan"]
+
+
+#find a composition with the maximum amount of emblems used for traits, where it is not redundant (like syndicate 6)
+
+#it should be in order of 
+# 1. the amount of emblems used
+# 2. the amount of traits active
+# 3. the sum of the champions' costs
+
+#filter the list of compositions with the champions used
+
+
+#filter the list of compositions with the team_size
